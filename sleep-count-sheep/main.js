433a547e8c5ff577e8d8987a3a4f2821936aefa3
Raey Abera <raeyabera@Raeys-MBP.fios-router.home>
2019-03-18 21:50:50 -0400

    second commit

--- sleep-count-sheep/main.js
+++ sleep-count-sheep/main.js
@@ -11,7 +11,7 @@ var countSheep = function (num){
   return sum;
 }
 
-// Sample Tests:
+// Sample Test:
 // Test.assertEquals(countSheep(1), "1 sheep...");
 // Test.assertEquals(countSheep(2), "1 sheep...2 sheep...");
 // Test.assertEquals(countSheep(3), "1 sheep...2 sheep...3 sheep...");
