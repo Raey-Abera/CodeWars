ae5d0ba1919506f07e5d0a2739d6942f8ad65800
Raey Abera <ab.raey@gmail.com>
2019-04-08 22:01:24 -0400

    first commit

---
+++ return/main.js
@@ -0,0 +1,14 @@
+// In thiss assignment you are given a number and have to make it negative.
+
+function makeNegative(num) {
+    // Code?
+
+    if(num>0){
+    return num * -1;
+
+    }else{
+
+    return num;
+
+    }
+  }
