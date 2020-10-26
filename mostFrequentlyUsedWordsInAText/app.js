717f33282f9caa0ea5edefb485f1690150783bd9
Raey-Abera <ab.raey@gmail.com>
2020-10-26 14:07:43 -0400

    added tests

--- mostFrequentlyUsedWordsInAText/app.js
+++ mostFrequentlyUsedWordsInAText/app.js
@@ -54,22 +54,39 @@ const findMaxValue = hash => {
 }
 
 const topThreeWords = (text) => {
-
     const result = []
-    const textArray = text.split(/[^A-Z0-9']/ig).filter(s => s !== '');
+    const textArray = text.split(/[^A-Z0-9']/ig).filter(s => s !== '')
 
     if (textArray.length < 2) return textArray[0] === '\'' ? [] : textArray
     if (textArray.length === 3) return removeDuplicates(textArray).reverse()
 
-    const textHash = createHash(textArray);
+    const textHash = createHash(textArray)
 
     for (let i = 0; i < 3; i++) {
         const largestChar = findMaxValue(textHash)
         result.push(largestChar)
         delete textHash[largestChar]
     }
-
     return result
 }
 
+console.log(topThreeWords("a a a  b  c c  d d d d  e e e e e"))//['e','d','a']
+  
+console.log(topThreeWords("a a c b b"))//['a','b','c']
+  
+console.log(topThreeWords("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e"))//['e','ddd','aa']
 
+console.log(topThreeWords("  //wont won't won't "))//["won't", "wont"]
+  
+console.log(topThreeWords("  , e   .. "))//["e"]
+
+console.log(topThreeWords("  ...  "))//[]
+
+console.log(topThreeWords("  '  "))//[]
+  
+console.log(topThreeWords(`In a village of La Mancha, the name of which I have no desire to call to
+mind, there lived not long since one of those gentlemen that keep a lance
+in the lance-rack, an old buckler, a lean hack, and a greyhound for
+coursing. An olla of rather more beef than mutton, a salad on most
+nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra
+on Sundays, made away with three-quarters of his income.`))//['a','of','on']
