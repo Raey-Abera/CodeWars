321094343b31cd267c41669c83eeab63e92cbe44
Raey-Abera <ab.raey@gmail.com>
2020-11-11 09:43:14 -0500

    refactored

--- bit_counting/main.js
+++ bit_counting/main.js
@@ -2,14 +2,22 @@
 
 //Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
 
-let countBits = function(n) {
-  // Program Me
-  let x= n.toString(2)// base two returned
-  let counter= 0;
-  for (i =0; i < x.length; i++){ //iterating through the array to find the length
-    if (x[i] === '1'){ // comparing the value of sub i to the sting passed through
-      counter++ // incrementing
-    }
-  }
-  return counter;
-};
+// const countBits = num => {
+//   let x = num.toString(2)// base two returned
+//   let counter = 0;
+//   for (i = 0; i < x.length; i++) { //iterating through the array to find the length
+//     if (x[i] === '1') { // comparing the value of sub i to the sting passed through
+//       counter++ 
+//     }
+//   }
+//   return counter;
+// };
+
+const countBits = num => num.toString(2).split('0').join('').length;
+
+console.log(countBits(0))//0
+console.log(countBits(4))//1  
+console.log(countBits(7))//3  
+console.log(countBits(9))//2  
+console.log(countBits(10))//2 
+
