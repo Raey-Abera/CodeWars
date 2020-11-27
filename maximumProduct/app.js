8db2608d0d51b2871aae27d7ebb950fb56a7edd3
Raey-Abera <ab.raey@gmail.com>
2020-11-26 23:00:04 -0500

    first commit

--- maximumProduct/app.js
+++ maximumProduct/app.js
@@ -1,10 +1,10 @@
-// Task
+// Task:
 // Given an array of integers , Find the maximum product obtained from multiplying 2 adjacent numbers in the array.
 
-// Notes
-// Array/list size is at least 2.
+// Notes:
+// Array/list size is at least 2
 
-// Array/list numbers could be a mixture of positives, negatives also zeroes .
+// Array/list numbers could be a mixture of positives, negatives also zeroes 
 
 // Input >> Output Examples
 // adjacentElementsProduct([1, 2, 3]); ==> return 6
