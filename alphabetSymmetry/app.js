4a3034356c60bb88290a6c9c0cbaffa283d884e8
Raey-Abera <ab.raey@gmail.com>
2021-02-11 17:12:12 -0500

    refactoring alphabet symmetry solution

--- alphabetSymmetry/app.js
+++ alphabetSymmetry/app.js
@@ -9,24 +9,31 @@ https://www.codewars.com/kata/59d9ff9f7905dfeed50000b0
 
 // Input will consist of alphabet characters, both uppercase and lowercase. No spaces.
 
-const solve = arr => {  
+//refactored solution 
+const solve = arr => {
+    let alphabeth = 'abcdefghijklmnopqrstuvwxyz'
+    return arr.map(char => char.toLowerCase().split('').filter((el, i) => i === alphabeth.indexOf(el)).length)
+}
+
+//slower solution 
+const solve = arr => {
 
     let map = 'abcdefghijklmnopqrstuvwxyz'
     let result = []
     let count = 0
-    
-    for(let i = 0; i < arr.length; i++){
-      for(let j= 0; j < arr[i].length; j++){
-        
-        const char = arr[i][j].toLowerCase()
-        if(char === map[j]){
-          count++
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+
+            const char = arr[i][j].toLowerCase()
+            if (char === map[j]) {
+                count++
+            }
         }
-      }
-      
-      result.push(count)
-      count= 0
+
+        result.push(count)
+        count = 0
     }
-    
+
     return result
-  }
+}
