12db7a94e8bb652ba1e5208f7299551d32e4b986
Raey-Abera <ab.raey@gmail.com>
2021-04-20 14:54:28 -0400

    adding codewars link

--- funWithLists:IndexOf/app.js
+++ funWithLists:IndexOf/app.js
@@ -1,3 +1,4 @@
+https://www.codewars.com/kata/581c6b075cfa83852700021f/train/javascript
 // Implement the method indexOf (index_of in PHP), which accepts a linked list (head) and a value, and returns the index (zero based) of the first occurrence of that value if exists, or -1 otherwise.
 
 // For example: Given the list: 1 -> 2 -> 3 -> 3, and the value 3, indexOf / index_of should return 2.
